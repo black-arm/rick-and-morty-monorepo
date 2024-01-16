35f9c8c940845b6a8b7ea539b4650298370a6098
Antonio Basile <antoniobasile2@eng.it>
2024-01-16 23:57:12 +0100

    test: test on spinner loading interceptor

--- apps/rick-and-morty-monorepo/src/app/core/interceptors/spinner-loading.interceptor.spec.ts
+++ apps/rick-and-morty-monorepo/src/app/core/interceptors/spinner-loading.interceptor.spec.ts
@@ -1,17 +1,49 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
-
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { spinnerLoadingInterceptor } from './spinner-loading.interceptor';
+import { GeneralStore } from '../domain/general-store/general-store';
+
 
 describe('spinnerLoadingInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
-    TestBed.runInInjectionContext(() => spinnerLoadingInterceptor(req, next));
+
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let generalStore: any;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
+      providers: [
+        provideHttpClientTesting(),
+        provideHttpClient(withInterceptors([spinnerLoadingInterceptor])),
+        GeneralStore
+      ]
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    generalStore = TestBed.inject(GeneralStore)
   });
 
   it('should be created', () => {
-    expect(interceptor).toBeTruthy();
+    expect(httpClient).toBeTruthy();
+    expect(httpTestingController).toBeTruthy();
   });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  })
+  it('should spinner to true', () => {
+    const url = 'https://localhost:8080/mockEndpoint';
+    httpClient.get(url).subscribe(() =>{
+      //expect(generalStore.isLoading()).toBeFalsy();
+    });
+    expect(generalStore.isLoading()).toBeTruthy();
+    //const req = httpTestingController.expectOne('https://localhost:8080/mockEndpoint');
+    //req.flush([], { status: 200 });
+
+  })
+
 });
